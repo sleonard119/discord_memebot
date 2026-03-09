e1682338d0590a7ba5fc1e160a4e467e9925ac4e
sleonard119 <seannicolasleo@gmail.com>
2026-03-09 15:58:28 +0800

    Adding new files to the Repository on GitHub

---
+++ bot.py
@@ -0,0 +1,35 @@
+import discord
+from dotenv import dotenv_values
+import requests
+import json
+
+def get_meme():
+    response = requests.get('https://meme-api.com/gimme')
+    json_data = json.loads(response.text)
+    return json_data['url']
+
+config = dotenv_values(".env")
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+        
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('Hello'):
+            await message.channel.send('Hello World!')
+
+        if message.content.startswith('$meme'):
+            await message.channel.send(get_meme())
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(config['Token_Code'])
+
+
+
+
